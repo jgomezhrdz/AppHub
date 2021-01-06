882c3f76186a29de3236e87b1a12c923133e50d5
jgomezhrdz <68118150+jgomezhrdz@users.noreply.github.com>
2021-01-06 12:46:23 +0100

    Revert "Update youtube.service.ts"
    This reverts commit 74287d6b5dda31fb2f3672368096ffe6a97031ec.

--- AppHub/src/app/youtube/youtube.service.ts
+++ AppHub/src/app/youtube/youtube.service.ts
@@ -15,4 +15,10 @@ export class YoutubeService {
     const url = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=+"+_keyWord+"&type=video&videoCaption=closedCaption&key="+this.API_KEY+""
     return this.http.get<any>(url)
   }
+  downloadVideo(){
+    const fs = require('fs')
+    const youtubedl = require('youtube-dl')
+    const video = youtubedl('http://www.youtube.com/watch?v=90AiXO1pAiA')
+    video.pipe(fs.createWriteStream('myvideo.mp4'))
+  }
 }
